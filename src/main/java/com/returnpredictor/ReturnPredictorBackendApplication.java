6bd0f828d9873e868d6770db9559865c04c8cd1b
Rupesh Vislawath <rupeshvislawath@gmail.com>
2025-07-26 20:17:40 +0530

    Initial Spring Boot project setup with application.yml

---
+++ src/main/java/com/returnpredictor/ReturnPredictorBackendApplication.java
@@ -0,0 +1,13 @@
+package com.returnpredictor;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ReturnPredictorBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ReturnPredictorBackendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/returnpredictor/ReturnPredictorBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.returnpredictor;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ReturnPredictorBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
