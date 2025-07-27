d41ccf2bee74c1cef18ed4a6a19113ea4ce097d1
Rupesh Vislawath <rupeshvislawath@gmail.com>
2025-07-27 14:43:45 +0530

    RP-31 [Rupesh] Adding Spotless check to the project.

--- src/main/java/com/returnpredictor/ReturnPredictorBackendApplication.java
+++ src/main/java/com/returnpredictor/ReturnPredictorBackendApplication.java
@@ -6,8 +6,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class ReturnPredictorBackendApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(ReturnPredictorBackendApplication.class, args);
-	}
-
+  public static void main(String[] args) {
+    SpringApplication.run(ReturnPredictorBackendApplication.class, args);
+  }
 }
--- src/test/java/com/returnpredictor/ReturnPredictorBackendApplicationTests.java
+++ src/test/java/com/returnpredictor/ReturnPredictorBackendApplicationTests.java
@@ -6,8 +6,6 @@ import org.springframework.boot.test.context.SpringBootTest;
 @SpringBootTest
 class ReturnPredictorBackendApplicationTests {
 
-	@Test
-	void contextLoads() {
-	}
-
+  @Test
+  void contextLoads() {}
 }
